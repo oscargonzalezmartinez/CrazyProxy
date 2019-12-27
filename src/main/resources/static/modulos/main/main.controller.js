18c7e96cecc043b668c84a0a9de69dbb576fd39e
OSCAR GONZALEZ MARTINEZ <OGOMAR01@caser.local>
2019-12-27 13:38:44 +0100

    Clean up

--- src/main/resources/static/modulos/main/main.controller.js
+++ src/main/resources/static/modulos/main/main.controller.js
@@ -1,80 +1,46 @@
-app.controller('MainController', 
-	function ($scope,$http,$rootScope,$timeout,MainService) {
-	
-    var socket = new SockJS('/wsstats');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        //setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/stats', function (event) {
-        	console.debug("WS receive update");
-       	 console.log(event.body);
-        	 
-       	  	$scope.info = angular.fromJson(event.body);
-       	  	$scope.$apply();
-    	  
-        });
-    });
+app.controller('MainController', function($scope, $http, $rootScope, $timeout,
+		MainService) {
 
+	var socket = new SockJS('/wsstats');
+	stompClient = Stomp.over(socket);
+	stompClient.connect({}, function(frame) {
 
-	
-	               $scope.refreshInfoFromServer = function(data) {
-	            	   $scope.info = data;
-	               }
-	               
-	$scope.refreshInfo = function() {
-		MainService.getInfo().then(
-				function (response) {
-						$scope.info = response.data;
-						//$scope.info.delay = 33;
-						if ($scope.info.request!=0){
-							var result = parseFloat($scope.info.executionTime / $scope.info.request).toFixed(2);
-							if (!isNaN(result)){
-								$scope.mediaEjecucion =  result;
-							}
-						}
-						
-					}, function (response) {
-						console.debug("search ko");								  
-						// $rootScope.error = response.status + " >> " +
-						// response.statusText;
-						// $window.alert(response.status + " >> " +
-						// response.statusText);
-					}
-				);		
-	}
-	
-	//$scope.refreshInfo();
-	
-		$scope.setErrorThreshold = function() {
-			   MainService.setErrorThreshold($scope.info.errorThreshold).then(function (response) {
-							// $scope.detail = response.data;
-						
-							  }, function (response) {
-								  	console.debug("login ko");								  
-								// $rootScope.error = response.status + " >> " +
-								// response.statusText;
-								// $window.alert(response.status + " >> " +
-								// response.statusText);
-							  }
-							  );
-		};// setErrorThreshold
-		
-		   $scope.setDelay = function() {
-			   MainService.setDelay($scope.info.delay).then(function (response) {
-							// $scope.detail = response.data;
-						
-							  }, function (response) {
-								  	console.debug("login ko");								  
-								// $rootScope.error = response.status + " >> " +
-								// response.statusText;
-								// $window.alert(response.status + " >> " +
-								// response.statusText);
-							  }
-							  );
-		};// setErrorThreshold
-		
-	}// function
+		console.log('Connected: ' + frame);
+		stompClient.subscribe('/topic/stats', function(event) {
+			console.debug("WS receive update");
+			console.log(event.body);
 
+			$scope.info = angular.fromJson(event.body);
+			if ($scope.info.request != 0) {
+				var result = parseFloat(
+						$scope.info.executionTime / $scope.info.request)
+						.toFixed(2);
+				if (!isNaN(result)) {
+					$scope.mediaEjecucion = result;
+				}
+			}
+
+			$scope.$apply();
+
+		});//stompClient
+	});
+
+	$scope.setErrorThreshold = function() {
+		MainService.setErrorThreshold($scope.info.errorThreshold).then(
+				function(response) {
+
+				}, function(response) {
+					console.debug("setErrorThreshold ko");
+				});
+	};// setErrorThreshold
+
+	$scope.setDelay = function() {
+		MainService.setDelay($scope.info.delay).then(function(response) {
+		}, function(response) {
+			console.debug("setDelay ko");
+		});
+	};// setDelay
+
+}// MainController
 
 );
--- src/main/resources/static/modulos/main/main.services.js
+++ src/main/resources/static/modulos/main/main.services.js
@@ -1,34 +1,26 @@
-app.service("MainService", function($http){
-			
-			this.getInfo = function(){
-								return $http({
-										method: 'GET',
-										url: '/info/'
-										});
-			}//getRecords
-			
-			this.getDetail = function(id){
-								return $http({
-										method: 'GET',
-										url: 'http://localhost:9000/get/'+id
-										});
-			}//getDetail
-			
-			this.setErrorThreshold = function(errorTH){
-								return $http({
-										method: 'POST',
-										url: '/settings/errorTH/'+errorTH
-										});
-			}//setErrorThreshold
-			
-			this.setDelay = function(delay){
-				return $http({
-						method: 'POST',
-						url: '/settings/delay/'+delay
-						});
-}//setErrorThreshold
-			
-		}//myHttpServices
+app.service("MainService", function($http) {
 
+	this.getInfo = function() {
+		return $http({
+			method : 'GET',
+			url : '/info/'
+		});
+	}//getInfo
 
-);	
+	this.setErrorThreshold = function(errorTH) {
+		return $http({
+			method : 'POST',
+			url : '/settings/errorTH/' + errorTH
+		});
+	}//setErrorThreshold
+
+	this.setDelay = function(delay) {
+		return $http({
+			method : 'POST',
+			url : '/settings/delay/' + delay
+		});
+	}//setDelay
+
+}//MainService
+
+);
